1ab25df1c9e391ede3191a3e0b84d5ae4cdc8a99
7_5P <76pollet@gmail.com>
2019-09-28 18:00:24 +0300

    test that cards can be compared and ranked correctly

--- test/poker_test.exs
+++ test/poker_test.exs
@@ -1,25 +1,25 @@
 defmodule PokerTest do
   use ExUnit.Case
   alias Poker.CardClassification
+  alias Poker.CardRanking
   alias Poker.ComparisonValue
-  doctest Poker
 
-  test "greets the world" do
-    assert Poker.hello() == :world
-  end
+  doctest CardClassification
+  doctest CardRanking
+  doctest ComparisonValue
 
   test "the partial order of a card can be determined" do
-    assert CardClassification.partial_order_of_card("2H 3D 5S 9C KD") == "High Card"
-    assert CardClassification.partial_order_of_card("2S 8S AS QS 3S") == "Flush"
-    assert CardClassification.partial_order_of_card("2H 3D 3S 9C KD") == "Pair"
-    assert CardClassification.partial_order_of_card("2H 3D 3S 9C 9D") == "Two Pair"
-    assert CardClassification.partial_order_of_card("2H 3D 9S 9C 9D") == "Three of a kind"
-    assert CardClassification.partial_order_of_card("2H 9D 9S 9C 9D") == "Four of a kind"
-    assert CardClassification.partial_order_of_card("2H 2D 9S 9C 9D") == "Full hand"
-    assert CardClassification.partial_order_of_card("9H 9D 9S 9C 9D") == "Straight"
+    assert CardClassification.partial_order_of_hand("2H 3D 5S 9C KD") == "High Card"
+    assert CardClassification.partial_order_of_hand("2S 8S AS QS 3S") == "Flush"
+    assert CardClassification.partial_order_of_hand("2H 3D 3S 9C KD") == "Pair"
+    assert CardClassification.partial_order_of_hand("2H 3D 3S 9C 9D") == "Two Pair"
+    assert CardClassification.partial_order_of_hand("2H 3D 9S 9C 9D") == "Three of a kind"
+    assert CardClassification.partial_order_of_hand("2H 9D 9S 9C 9D") == "Four of a kind"
+    assert CardClassification.partial_order_of_hand("2H 2D 9S 9C 9D") == "Full hand"
+    assert CardClassification.partial_order_of_hand("9H 9D 9S 9C 9D") == "Straight"
   end
 
-  test "the first integer value to be used in comparing two hands can be retrieved from a hand" do
+  test "The first integer value to be used in comparing two hands can be retrieved from a hand" do
     assert ComparisonValue.get_comparison_value("2H 3D 5S 9C KD") == 13
     assert ComparisonValue.get_comparison_value("2H 3D 5S 9C 7D") == 9
     assert ComparisonValue.get_comparison_value("2H 5D 5S 9C 8D") == 5
@@ -27,5 +27,16 @@ defmodule PokerTest do
     assert ComparisonValue.get_comparison_value("2H 9D 9S 9C 9D") == 9
     assert ComparisonValue.get_comparison_value("2S 8S AS QS 3S") == 14
     assert ComparisonValue.get_comparison_value("2H 2D 9S 9C 9D") == 9
+  end
+
+  test "Cards can be ranked" do
+    assert CardRanking.rank_hands("2H 3D 5S 9C KD", "2C 3H 4S 8C AH") ==
+             "White wins - High Card: Ace"
+
+    assert CardRanking.rank_hands("2H 3D 5S 9C KD", "2C 3H 4S 8C KH") ==
+             "Black wins - High Card: 9"
+
+    assert CardRanking.rank_hands("2H 4S 4C 3D 4H", "2S 8S AS QS 3S") == "White wins - Flush"
+    assert CardRanking.rank_hands("2H 3D 5S 9C KD", "2D 3H 5C 9S KH") == "Tie"
   end
 end
