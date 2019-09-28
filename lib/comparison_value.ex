78bafc55871682210f03c902d5735b8cf5f85d3c
7_5P <76pollet@gmail.com>
2019-09-28 14:10:03 +0300

    get the values to be compared for each hand

--- lib/comparison_value.ex
+++ lib/comparison_value.ex
@@ -1,4 +1,23 @@
 defmodule Poker.ComparisonValue do
+  alias Poker.CardClassification
+
+  @moduledoc """
+  this module contain functions that get the values to be compared from values of the cards in a single hand
+  """
+
+  def get_comparison_value(hand) do
+    partial_order = CardClassification.partial_order_of_card(hand)
+    {_suits, values} = CardClassification.values_and_suits_of_cards(hand)
+
+    case partial_order do
+      partial_order when partial_order in ["Straight", "Straight Flush", "Flush", "High Card"] ->
+        values |> CardClassification.integer_values() |> highest_value()
+
+      _ ->
+        values |> CardClassification.integer_values() |> multiple_card_value()
+    end
+  end
+
   @doc """
   highest_value/1 returns the value of the highest value card in a hand. This 
   is used for the following partial orders: straight, flush, straight flush and high card.
